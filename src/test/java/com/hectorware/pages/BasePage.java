4320c33f808584715c9503ce63db3db79b70cd12
KhrystynaN <chrystylka@gmail.com>
2023-03-07 17:45:31 -0500

    US05 Add files to Favorites is finished

--- src/test/java/com/hectorware/pages/BasePage.java
+++ src/test/java/com/hectorware/pages/BasePage.java
@@ -1,11 +1,17 @@
 package com.hectorware.pages;
 
 import com.hectorware.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    public void navigateTo(String module){
+        WebElement linkToPage = Driver.getDriver().findElement(By.xpath("(//a[@aria-label='" + module + "'])[1]"));
     }
 
 
