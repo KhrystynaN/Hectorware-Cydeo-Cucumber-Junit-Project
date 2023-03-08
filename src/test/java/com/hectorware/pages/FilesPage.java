c2e92227db1d691370f514e6f4a0f4900b54cf1b
Marko'sMac <Markolucas93@yahoo.com>
2023-03-08 13:43:31 -0500

    US08 and US13 Completed Scenarios and Step Definitions with the following WebElements on the Files page.

--- src/test/java/com/hectorware/pages/FilesPage.java
+++ src/test/java/com/hectorware/pages/FilesPage.java
@@ -1,5 +1,43 @@
 package com.hectorware.pages;
 
+import io.cucumber.java.zh_cn.假如;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class FilesPage extends BasePage{
+
+
+    // USER STORY 08
+    @FindBy(xpath = "//span[@class=\"icon icon-add\"]")
+    public WebElement uploadBar;
+
+    @FindBy(xpath = "//*[contains(text(),'New folder')]")
+    public WebElement selectNewFolder;
+
+    @FindBy(xpath = "//input[@value=\"New folder\"]")
+    public WebElement newFolderText;
+
+    @FindBy(xpath = "//input[@class=\"icon-confirm\"]")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//span[text()='Java folder']")
+    public WebElement javaFolder;
+
+    // USER STORY 13
+
+    @FindBy(xpath = "//a[@class=\"header-menu__trigger\"]")
+    public WebElement searchButton;
+
+    @FindBy(xpath = "//input[@type=\"search\"]")
+    public WebElement searchTextInput;
+
+    @FindBy(xpath = "//ul[@aria-label=\"Files\"]")
+    public WebElement filesResult;
+
+    @FindBy(xpath = "//ul[@aria-label=\"Apps\"]")
+    public WebElement appsResult;
+
+
+
 
 }
---
+++ src/test/java/com/hectorware/step_definitions/US13_SearchAnyItem_Step_Definitions.java
@@ -0,0 +1,48 @@
+package com.hectorware.step_definitions;
+
+import com.hectorware.pages.FilesPage;
+import com.hectorware.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class US13_SearchAnyItem_Step_Definitions {
+
+
+    FilesPage filesPage = new FilesPage();
+
+    @When("user click on search icon")
+    public void user_click_on_search_icon() {
+        System.out.println("User click on search icon");
+        filesPage.searchButton.click();
+
+    }
+    @And("user type selenium in search")
+    public void user_type_selenium_in_search() {
+        System.out.println("User search for selenium");
+        filesPage.searchTextInput.sendKeys("selenium" + Keys.ENTER);
+
+    }
+    @Then("verify, the app is displaying related result")
+    public void verify_the_app_is_displaying_related_result() {
+        System.out.println("User verify if app is displaying search result");
+        System.out.println("filesPage.filesResult.isDisplayed() = " + filesPage.filesResult.isDisplayed());
+        int expectedNumberOfFiles = 2;
+        List<WebElement> actualNumberOfFiles = Driver.getDriver().findElements(By.xpath("//ul[@aria-label=\"Files\"]"));
+        int actualNumber = actualNumberOfFiles.size();
+
+        Assert.assertEquals(expectedNumberOfFiles,actualNumber);
+
+    }
+
+
+
+
+}
