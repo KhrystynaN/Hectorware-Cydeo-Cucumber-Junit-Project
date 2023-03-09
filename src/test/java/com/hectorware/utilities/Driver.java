a6f25cf7943d288bd9f59193d3840fea273f28cb
mohammedhanoune <moe.hanoune@gmail.com>
2023-03-08 21:46:45 -0500

    Driver updated

--- src/test/java/com/hectorware/utilities/Driver.java
+++ src/test/java/com/hectorware/utilities/Driver.java
@@ -2,6 +2,7 @@ package com.hectorware.utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -39,6 +40,8 @@ public class Driver {
 
                 case "chrome":
                     //WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--remote-allow-origins=*");
                     driverPool.set(new ChromeDriver());
                     driverPool.get().manage().window().maximize();
                     driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
