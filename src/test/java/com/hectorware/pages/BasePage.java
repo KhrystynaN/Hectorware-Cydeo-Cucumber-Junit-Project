e98d52e85b19898671e1097f6a841b8bc25c3e99
mohammedhanoune <moe.hanoune@gmail.com>
2023-03-08 22:36:43 -0500

    US06-create a user story 1

--- src/test/java/com/hectorware/pages/BasePage.java
+++ src/test/java/com/hectorware/pages/BasePage.java
@@ -1,12 +1,23 @@
 package com.hectorware.pages;
 
 import com.hectorware.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class BasePage {
+public class BasePage {
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+   public void navigateTo(String module){
+        WebElement linkToPage = Driver.getDriver().findElement(By.xpath("(//a[@aria-label='" + module + "'])[1]"));
+
+        linkToPage.click();
+}
+
 
 
 }
